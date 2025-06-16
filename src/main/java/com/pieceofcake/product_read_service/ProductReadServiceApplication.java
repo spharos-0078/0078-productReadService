8523300157bafa7b6d95007855100318523b75b2
LeeYeongin <youngin5757@gmail.com>
2025-06-16 13:53:14 +0900

    [chore] application.yml 수정, eureka 연결
    - mongodb 연결 설정 변수화
    - eureka dependency 추가
    - application 에 @EnableDiscoveryClient 선언
    - gitignore 수정
    
    #1

--- src/main/java/com/pieceofcake/product_read_service/ProductReadServiceApplication.java
+++ src/main/java/com/pieceofcake/product_read_service/ProductReadServiceApplication.java
@@ -2,9 +2,9 @@ package com.pieceofcake.product_read_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductReadServiceApplication {
 
