e3a5ec9efa9ad86cce99b1621f7b2d446e99f026
Eunseo <kendyjenny@naver.com>
2025-06-16 14:55:39 +0900

    [Feat] CICD 초기 설정 완료

--- src/main/java/com/pieceofcake/product_read_service/common/config/SwaggerConfig.java
+++ src/main/java/com/pieceofcake/product_read_service/common/config/SwaggerConfig.java
@@ -29,14 +29,14 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .addSecurityItem(securityRequirement)
                 .components(components)
-                .addServersItem(new Server().url("/piece-service"))
+                .addServersItem(new Server().url("/product-read-service"))
                 .info(apiInfo());
     }
 
     private Info apiInfo() {
         return new Info()
-                .title("Piece Service API DOCS")
-                .description("piece-service API 테스트를 위한 Swagger UI")
+                .title("Product Read Service API DOCS")
+                .description("product-read-service API 테스트를 위한 Swagger UI")
                 .version("1.0.0");
     }
 
